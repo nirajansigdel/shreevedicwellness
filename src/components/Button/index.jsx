6325f6d85b2f16d4d687aaf72c6447ae63a02553
miroz devkota <mirozxy@gmail.com>
2023-10-12 23:07:22 +0545

    fixes

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 
-function Button({ children }) {
+function Button({ children, onClick, type }) {
+  if (type == "secondary") {
+    return (
+      <button className="bg-[#22C55E] text-[16px] font-medium text-white rounded-md w-full flex items-center justify-center p-4 cursor-pointer">
+        {children}
+      </button>
+    );
+  }
+
   return (
-    <div className="bg-[#22C55E] text-[16px] font-medium text-white rounded-md w-full flex items-center justify-center p-4 cursor-pointer">
+    <button
+      onClick={onClick}
+      className="bg-[#22C55E] text-[16px] font-medium text-white rounded-md w-full flex items-center justify-center p-4 cursor-pointer"
+    >
       {children}
-    </div>
+    </button>
   );
 }
 
