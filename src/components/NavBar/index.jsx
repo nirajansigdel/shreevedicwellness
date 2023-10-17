5b4ea547f25d90f899d37b6b103f883b4201d080
miroz devkota <mirozxy@gmail.com>
2023-10-17 09:39:15 +0545

    navbar work

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -6,27 +6,30 @@ import Logo from "./logo.png";
 
 function Index() {
   return (
-    <div className="w-full bg-[#7E8446] min-h-[120px] flex items-center justify-center">
-      <div className="flex items-center gap-10">
-        <Link href={"/registration"}>
-          <span className="text-white">Yoga</span>
-        </Link>
-        <Link href={"/registration"}>
-          <span className="text-white">Meditation</span>
-        </Link>
-        <span>
-          <Image src={Logo} alt="logo" />
-        </span>
+    <div className="w-full bg-white py-[22px] flex items-center justify-center">
+      <div className="flex w-full w-3/4 gap-5 flex flex-row justify-between">
+        <p>
+          <span className="text-[#4B5563] font-bold text-[24px]">YOGAS</span>
+          <span className="text-[#22C55E] font-bold text-[24px]">TA</span>
+        </p>
 
-        <Link href={"/registration"}>
-          <span className="text-white">Wellness</span>
-        </Link>
-        <Link href={"/registration"}>
-          <span className="text-white">Learning</span>
-        </Link>
-        <Link href={"/registration"}>
-          <span className="text-white">Events</span>
-        </Link>
+        <div className="flex items-center gap-10">
+          <Link href={"/"}>
+            <span className="text-[#4B5563] font-medium">Home</span>
+          </Link>
+          <Link href={"/"}>
+            <span className="text-[#4B5563] font-medium">Courses</span>
+          </Link>
+
+          <Link href={"/"}>
+            <span className="text-[#4B5563] font-medium">Pricing</span>
+          </Link>
+          <Link href={"/"}>
+            <span className="text-white rounded-md px-4 py-2 font-medium bg-[#22C55E]">
+              Join Class
+            </span>
+          </Link>
+        </div>
       </div>
     </div>
   );
